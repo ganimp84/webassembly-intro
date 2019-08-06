07acd5e1d2e35d876e2590ac1c5a83220f894f2f
Gani Mohamed Parakadhullah <gani@sam-media.com>
2019-08-06 22:53:53 +0800

    Initial commit - Example 02

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+const importObject = {
+  env: {
+      logResult(result) {
+          console.log(result);
+      },
+  }
+}
+
 fetch('../out/main.wasm').then(response =>
     response.arrayBuffer()
-).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+).then(bytes => WebAssembly.instantiate(bytes, importObject)).then(results => {
     instance = results.instance;
     document.getElementById("container").textContent = instance.exports.add_one(41);
 }).catch(console.error);
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+extern {
+    fn logResult(i: i32);
+}
+
 #[no_mangle]
-pub extern "C" fn add_one(x: i32) -> i32 {
-    x + 1
+pub extern "C" fn add_one(x: i32) {
+    unsafe {
+        logResult(x + 1);
+    }
 }
