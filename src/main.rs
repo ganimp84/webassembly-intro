628f8ec461b659e1dfcfe4e2c9686df52a8348ce
Gani Mohamed Parakadhullah <gani@sam-media.com>
2019-08-06 23:23:07 +0800

    Initial commit - Example 03

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 #[no_mangle]
-pub extern "C" fn add_one(x: i32) -> i32 {
-    x + 1
+pub extern "C" fn getWelcomeMessage() -> *mut c_char {
+    CString::new("Hello from Rust!")
+        .unwrap()
+        .into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc_str(ptr: *mut c_char) {
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    }
 }
